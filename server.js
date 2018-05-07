0d88ad95ce2de7e7f571c65b9f2fd7db1a884382
Denisson <denisson_almeida@yahoo.com.br>
2018-05-07 14:05:46 -0300

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-var express = require('express'),
-    port = process.env.PORT || 3000,
-    //morgan = require('morgan'),
-    path = require('path'),
-    app = express();
+const express = require("express"),
+  PORT = process.env.PORT || 3000,
+  //morgan = require('morgan'),
+  path = require("path"),
+  app = express();
 
-// Create our app
-var app = express();
+// app.use(function(req, res, next) {
+//   if (req.headers["x-forwarded-proto"] === "http") {
+//     next();
+//   } else {
+//     res.redirect("http://" + req.hostname + req.url);
+//   }
+// });
 
 //app.use(express.static('node_modules/uikit/'));
-app.use('/css',express.static(path.resolve(__dirname, 'node_modules/uikit/dist/css')));
-app.use('/js',express.static(path.resolve(__dirname, 'node_modules/uikit/dist/js')));
-app.use('/dist',express.static(path.resolve(__dirname, 'dist')));
-app.use(express.static('public'));
+app.use(
+  "/css",
+  express.static(path.resolve(__dirname, "node_modules/uikit/dist/css"))
+);
+app.use(
+  "/js",
+  express.static(path.resolve(__dirname, "node_modules/uikit/dist/js"))
+);
+app.use("/dist", express.static(path.resolve(__dirname, "dist")));
+app.use(express.static("public"));
 
-
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function() {
+  console.log("Express server is up on port " + PORT);
 });
